77870db1b04dd68ef165e6ad1e27a8c52edc2d1e
teletha <teletha@users.noreply.github.com>
2022-01-18 15:09:57 +0000

    deploy: 740946f7a36465d3b79868fba55f303ce0b598be

---
+++ root.js
@@ -0,0 +1,37 @@
+const root = {
+	"docs": [],
+	"modules": [],
+	"packages": [
+		"kiss"
+	],
+	"types": [
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Commons",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Log4j2",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "Log4j2.Factory",
+			"packageName": "kiss",
+			"type": "Class"
+		},
+		{
+			"modifiers": {
+				"#": "java.util.Collections$UnmodifiableSet",},
+			"name": "SLF4J",
+			"packageName": "kiss",
+			"type": "Class"
+		}
+	]
+}
