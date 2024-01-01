9f863197f80de8f7ff6a4493adeb7ccf16fbfc37
Teletha <Teletha.T@gmail.com>
2024-01-01 15:43:36 +0900

    fix: update license

--- src/project/java/Project.java
+++ src/project/java/Project.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024 The CONJURE Development Team
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          https://opensource.org/licenses/MIT
+ */
 import javax.lang.model.SourceVersion;
 
 /*
